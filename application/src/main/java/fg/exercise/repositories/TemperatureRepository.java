a747eb777393f9ce8aa067641b646de6787d20e7
Petr Prouza <prouzpe3@fit.cvut.cz>
2021-08-04 15:14:48 +0200

    Add query to retrieve temperatures

--- application/src/main/java/fg/exercise/repositories/TemperatureRepository.java
+++ application/src/main/java/fg/exercise/repositories/TemperatureRepository.java
@@ -2,9 +2,15 @@ package fg.exercise.repositories;
 
 import fg.exercise.models.entities.Temperature;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.time.LocalTime;
+import java.util.List;
 
 
 public interface TemperatureRepository extends JpaRepository<Temperature, Long>, PagingAndSortingRepository<Temperature, Long> {
 
+    @Query("select id, localDate, localTime, temperature from Temperature where :lBound <= localTime and localTime <= :uBound order by localDate")
+    List<Temperature> findOrdered(LocalTime lBound, LocalTime uBound);
 }
