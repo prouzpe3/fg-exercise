8d084c231d9d3753cfc62560f9dadb73e3b57192
Petr Prouza <prouzpe3@fit.cvut.cz>
2021-08-05 15:26:26 +0200

    Add other tests of temperature repository

--- application/src/test/java/fg/exercise/repositories/TemperatureRepositoryTest.java
+++ application/src/test/java/fg/exercise/repositories/TemperatureRepositoryTest.java
@@ -1,17 +1,18 @@
 package fg.exercise.repositories;
 
 import fg.exercise.models.entities.Temperature;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static fg.exercise.utils.TemperatureFactory.createTemperature;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
@@ -21,12 +22,8 @@ public class TemperatureRepositoryTest {
     private TemperatureRepository temperatureRepository;
 
 
-    private Temperature morningTemperature1;
-    private Temperature afternoonTemperature1;
-    private Temperature eveningTemperature1;
-    private Temperature morningTemperature2;
-    private Temperature afternoonTemperature2;
-    private Temperature eveningTemperature2;
+    private Temperature temperature1;
+    private Temperature temperature2;
 
 
     @BeforeEach
@@ -35,84 +32,61 @@ public class TemperatureRepositoryTest {
     }
 
 
+    @AfterEach
+    void tearDown() {
+        temperatureRepository.deleteAll();
+    }
+
+
     private void setupTemperatures() {
         long id = 1L;
-        morningTemperature1
+        temperature1
                 = setupTemperature(id++, 10d,
                 LocalDate.of(2021, 8, 4),
                 LocalTime.of(10, 0, 0));
-        afternoonTemperature1
+        temperature2
                 = setupTemperature(id++, 15d,
                 LocalDate.of(2021, 8, 4),
                 LocalTime.of(15, 0, 0));
-        eveningTemperature1
-                = setupTemperature(id++, 20d,
-                LocalDate.of(2021, 8, 4),
-                LocalTime.of(20, 0, 0));
-        morningTemperature2
-                = setupTemperature(id++, 10d,
-                LocalDate.of(2021, 8, 5),
-                LocalTime.of(10, 0, 0));
-        afternoonTemperature2
-                = setupTemperature(id++, 15d,
-                LocalDate.of(2021, 8, 5),
-                LocalTime.of(15, 0, 0));
-        eveningTemperature2
-                = setupTemperature(id++, 20d,
-                LocalDate.of(2021, 8, 5),
-                LocalTime.of(20, 0, 0));
     }
 
 
     private Temperature setupTemperature(Long id, Double temperature, LocalDate date, LocalTime time) {
-        Temperature out = new Temperature();
-        out.setId(id);
-        out.setTemperature(temperature);
-        out.setLocalDate(date);
-        out.setLocalTime(time);
-        temperatureRepository.save(out);
-        return out;
+        Temperature temperature3 = createTemperature(id, temperature, date, time);
+        return temperatureRepository.save(temperature3);
     }
 
 
     @Test
-    public void findOrdered_restrictedRange_shouldReturnOnlyInRange() {
-        List<Temperature> temperatureList = temperatureRepository.findOrdered(LocalTime.of(10, 0), LocalTime.of(15, 0));
-        assertEquals(4, temperatureList.size());
-        assertTrue(temperatureList.stream().anyMatch(temperature -> temperature.getId().equals(morningTemperature1.getId())));
-        assertTrue(temperatureList.stream().anyMatch(temperature -> temperature.getId().equals(afternoonTemperature1.getId())));
-        assertTrue(temperatureList.stream().anyMatch(temperature -> temperature.getId().equals(morningTemperature2.getId())));
-        assertTrue(temperatureList.stream().anyMatch(temperature -> temperature.getId().equals(afternoonTemperature2.getId())));
+    public void findTemperatureByLocalDateAndLocalTime_exactDateTime_shouldReturnOne() {
+        assertTrue(temperatureRepository.findTemperatureByLocalDateAndLocalTime(LocalDate.of(2021, 8, 4), LocalTime.of(10, 0, 0)).isPresent());
     }
 
 
     @Test
-    public void findOrdered_restrictedRangeSpanningMidnight_morning_shouldReturnOnlyInRange() {
-        List<Temperature> temperatureList = temperatureRepository.findOrdered(LocalTime.of(22, 0), LocalTime.of(12, 0));
-        assertEquals(2, temperatureList.size());
-        assertTrue(temperatureList.stream().anyMatch(temperature -> temperature.getId().equals(morningTemperature1.getId())));
-        assertTrue(temperatureList.stream().anyMatch(temperature -> temperature.getId().equals(morningTemperature2.getId())));
+    public void findTemperatureByLocalDateAndLocalTime_noRecord_shouldReturnNone() {
+        assertFalse(temperatureRepository.findTemperatureByLocalDateAndLocalTime(LocalDate.of(2021, 10, 4), LocalTime.of(10, 0, 0)).isPresent());
     }
 
 
     @Test
-    public void findOrdered_restrictedRangeSpanningMidnight_evening_shouldReturnOnlyInRange() {
-        List<Temperature> temperatureList = temperatureRepository.findOrdered(LocalTime.of(17, 0), LocalTime.of(5, 0));
-        assertEquals(2, temperatureList.size());
-        assertTrue(temperatureList.stream().anyMatch(temperature -> temperature.getId().equals(eveningTemperature1.getId())));
-        assertTrue(temperatureList.stream().anyMatch(temperature -> temperature.getId().equals(eveningTemperature2.getId())));
+    public void save_duplicateDateTime_shouldThrow() {
+        Temperature duplicateTemperature = createTemperature(null, 10d, LocalDate.of(2021, 8, 4), LocalTime.of(10, 0, 0));
+        assertThrows(DataIntegrityViolationException.class, () ->
+                temperatureRepository.save(duplicateTemperature));
     }
 
 
     @Test
-    public void findOrdered_wholeDay_shouldReturnAll() {
-        List<Temperature> temperatureList = temperatureRepository.findOrdered(LocalTime.MIN, LocalTime.MAX);
-        assertEquals(6, temperatureList.size());
-        assertTrue(temperatureList.stream().anyMatch(temperature -> temperature.getId().equals(morningTemperature1.getId())));
-        assertTrue(temperatureList.stream().anyMatch(temperature -> temperature.getId().equals(morningTemperature2.getId())));
-        assertTrue(temperatureList.stream().anyMatch(temperature -> temperature.getId().equals(afternoonTemperature1.getId())));
-        assertTrue(temperatureList.stream().anyMatch(temperature -> temperature.getId().equals(afternoonTemperature2.getId())));
-        assertTrue(temperatureList.stream().anyMatch(temperature -> temperature.getId().equals(eveningTemperature1.getId())));
-        assertTrue(temperatureList.stream().anyMatch(temperature -> temperature.getId().equals(eveningTemperature2.getId())));
+    public void save_duplicateTime_shouldSave() {
+        Temperature temperature = createTemperature(null, 10d, LocalDate.of(2021, 10, 4), LocalTime.of(10, 0, 0));
+        temperatureRepository.save(temperature);
+    }
+
+
+    @Test
+    public void save_duplicateDate_shouldSave() {
+        Temperature temperature = createTemperature(null, 10d, LocalDate.of(2021, 8, 4), LocalTime.of(11, 0, 0));
+        temperatureRepository.save(temperature);
     }
 }
