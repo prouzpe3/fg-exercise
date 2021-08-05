f357b2ceb7ddf821881f4d8246660cfbf89f5140
Petr Prouza <prouzpe3@fit.cvut.cz>
2021-08-05 15:25:32 +0200

    Add util class for creating temperature objects

---
+++ application/src/test/java/fg/exercise/utils/TemperatureFactory.java
@@ -0,0 +1,20 @@
+package fg.exercise.utils;
+
+import fg.exercise.models.entities.Temperature;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+
+public class TemperatureFactory {
+
+    public static Temperature createTemperature(Long id, Double temperature, LocalDate date, LocalTime time) {
+        Temperature out = new Temperature();
+        out.setId(id);
+        out.setTemperature(temperature);
+        out.setLocalDate(date);
+        out.setLocalTime(time);
+        return out;
+    }
+
+}
