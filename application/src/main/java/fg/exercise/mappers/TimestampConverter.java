e93083f489061a3cca0d9aed1ab67aa103a96527
Petr Prouza <prouzpe3@fit.cvut.cz>
2021-08-05 13:51:01 +0200

    Datetime format doesn't accept millis

--- application/src/main/java/fg/exercise/mappers/TimestampConverter.java
+++ application/src/main/java/fg/exercise/mappers/TimestampConverter.java
@@ -12,7 +12,7 @@ import java.time.format.DateTimeParseException;
 public class TimestampConverter {
 
     public static final String DATE_FORMAT = "yyyy-MM-dd";
-    public static final String TIME_FORMAT = "HH:mm:ss.SS";
+    public static final String TIME_FORMAT = "HH:mm:ss";
     public static final String TIMESTAMP_FORMAT = DATE_FORMAT + " " + TIME_FORMAT;
 
     protected static DateTimeFormatter getTimestampFormatter() {
