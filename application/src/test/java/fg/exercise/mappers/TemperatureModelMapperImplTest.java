42c02f13982bfc3b61d5b1d3d2d9bb1d80da3a19
Petr Prouza <prouzpe3@fit.cvut.cz>
2021-08-05 15:38:19 +0200

    Add Temperature model mapper test

---
+++ application/src/test/java/fg/exercise/mappers/TemperatureModelMapperImplTest.java
@@ -0,0 +1,71 @@
+package fg.exercise.mappers;
+
+import fg.exercise.models.dtos.GetTemperatureDto;
+import fg.exercise.models.dtos.PostTemperatureDto;
+import fg.exercise.models.dtos.PutTemperatureDto;
+import fg.exercise.models.entities.Temperature;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest
+public class TemperatureModelMapperImplTest {
+
+    @Autowired
+    private TemperatureModelMapper temperatureModelMapper;
+
+
+    @Test
+    public void convertToEntity_postDto_shouldSetAllParams() {
+        Double temp = 12.4d;
+        PostTemperatureDto postTemperatureDto = new PostTemperatureDto();
+        postTemperatureDto.setTimestamp("2020-02-10 10:24:12");
+        postTemperatureDto.setTemperature(temp);
+
+        Temperature temperature = temperatureModelMapper.convertToEntity(postTemperatureDto);
+        assertEquals(null, temperature.getId());
+        assertEquals(temp, temperature.getTemperature());
+        assertEquals(LocalDate.of(2020, 2, 10), temperature.getLocalDate());
+        assertEquals(LocalTime.of(10, 24, 12), temperature.getLocalTime());
+    }
+
+
+    @Test
+    public void convertToEntity_putDto_shouldSetAllParamsIncludingId() {
+        Long id = 12L;
+        Double temp = 14.8d;
+        PutTemperatureDto putTemperatureDto = new PutTemperatureDto();
+        putTemperatureDto.setTimestamp("2020-02-10 10:24:12");
+        putTemperatureDto.setTemperature(temp);
+
+        Temperature temperature = temperatureModelMapper.convertToEntity(id, putTemperatureDto);
+        assertEquals(id, temperature.getId());
+        assertEquals(temp, temperature.getTemperature());
+        assertEquals(LocalDate.of(2020, 2, 10), temperature.getLocalDate());
+        assertEquals(LocalTime.of(10, 24, 12), temperature.getLocalTime());
+    }
+
+
+    @Test
+    public void convertToDto_shouldSetAllParams() {
+        Long id = 12L;
+        Double temp = 14.8d;
+        Temperature temperature = new Temperature();
+        temperature.setId(id);
+        temperature.setLocalDate(LocalDate.of(2020, 2, 10));
+        temperature.setLocalTime(LocalTime.of(10, 24, 12));
+        temperature.setTemperature(temp);
+
+        GetTemperatureDto dto = temperatureModelMapper.convertToDto(temperature);
+        assertEquals(id, dto.getId());
+        assertEquals(temp, dto.getTemperature());
+        assertEquals("2020-02-10 10:24:12", dto.getTimestamp());
+    }
+
+}
