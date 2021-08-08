7f01d6d27f3698b1d7da075c016beb5f0098fdda
JNO <novotny@fg.cz>
2021-08-08 14:34:38 +0200

    JNO - revision.

--- application/src/main/java/fg/exercise/services/TemperatureRangeService.java
+++ application/src/main/java/fg/exercise/services/TemperatureRangeService.java
@@ -5,6 +5,7 @@ import fg.exercise.models.entities.Temperature;
 import java.util.List;
 
 
+// TODO PPR - is there any reason for having interfaces with exactly single implementation?
 public interface TemperatureRangeService {
 
     List<Temperature> temperatureRangeGet(Double tempLBound, Double tempUBound, Integer timeLBound, Integer timeUBound) throws Exception;
